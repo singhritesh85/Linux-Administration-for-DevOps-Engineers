cb7ccfd9f968802f7e9956652ea90656f8d94b2d
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2026-01-17 10:35:22 +0530

    Delete terraform-ansible-tower/terraform-ansible-automation-azure directory

--- terraform-ansible-tower/terraform-ansible-automation-azure/main/output.tf
+++
@@ -1,4 +0,0 @@
-output "Ansible_automation_Azure_VMs_public_and_private_ip_postgresql_flible_servers_endpoint_and_application_gateway_public_ip" {
-  description = "Details of the Ansible-Automation Azure VMs Public IP, VMs Private IP, PostgreSQL Flexible Servers endpoint, ansible-automation ALB DNS Name"
-  value       = "${module.ansible_automation}"
-}
