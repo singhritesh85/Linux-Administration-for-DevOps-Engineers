a8614676b5bc2b2c5594c0aa8310ab938339b849
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2026-01-19 21:41:09 +0530

    Delete terraform-ansible-tower/terraform-ansible-automation-gcp directory

--- terraform-ansible-tower/terraform-ansible-automation-gcp/main/output.tf
+++
@@ -1,4 +0,0 @@
-output "gcp_vm_instance_private_and_static_ip_gcp_alb_static_ip_cloud_sql_mysql_details_and_cloud_dns_details" {
-  description = "Details of the Google Cloud VM Instance, Cloud SQL, GCP Application LoadBalancer and Cloud DNS"
-  value       = "${module.ansible_automation_platform}"
-}
